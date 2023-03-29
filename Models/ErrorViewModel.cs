e663ad2f7d203a53fdaa8a019ef2e44aa18b102f
alekvol <sane4st@yandex.ru>
2023-03-29 20:35:42 +0400

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResumeNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
